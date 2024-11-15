9423adb63e2d1941b8956594d94b9a08c965cdbd
chvpV888-2 <chavibhatia888@gmail.com>
2024-11-15 19:50:52 +0530

    Initial Commit

---
+++ app/build/generated/source/buildConfig/debug/com/cometchat/javasampleapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cometchat.javasampleapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.cometchat.javasampleapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "4.3.11";
+}
---
+++ app/src/main/java/com/cometchat/javasampleapp/AppConstants.java
@@ -0,0 +1,11 @@
+package com.cometchat.javasampleapp;
+
+public class AppConstants {
+
+    public static final String APP_ID = "263451de5d51a2a4";
+
+    public static final String AUTH_KEY = "75725a20bec62407389f955b37feda94e4018bfa";
+
+    public static final String REGION = "in";
+
+}
